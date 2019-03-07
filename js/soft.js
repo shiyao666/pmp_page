7c1ae8acb2040c1901e341d07eb34d32a118417b
shiyao666 <1102699514@qq.com>
2019-03-07 15:48:01 +0800

    pmp_style

--- js/soft.js
+++ js/soft.js
@@ -94,7 +94,7 @@ $(document).ready(function () {
             url: 'http://192.168.60.175:666/activity/enroll/commit',
             data: {
                 number: $("#recipient-phonenum").val(),
-                act_token: 'wD8VlzHoUt'
+                act_token: getCookie("page_token" + act_num + "")
             },
             success: function () {
 
@@ -154,6 +154,7 @@ $.ajax({
             $(".soft_open_qi").eq(i).find("li.soft_content4 span:eq(0)").text(event1.data[i].activity_content.title4);
             $(".soft_open_qi").eq(i).find("li.soft_content4 span:eq(1)").text(event1.data[i].activity_content.content4);
             $(".soft_open_qi").eq(i).find(".soft_set_info b").text(event1.data[i].max_number - event1.data[i].sign_up_number);
+            $(".soft_open_qi").eq(i).find(".soft_qi_num").text(event1.data[i].activity_content.head_name);
             setCookie("page_token" + [i] + "", Array(event1.data[i].page_token));
         }
 
@@ -163,22 +164,12 @@ $.ajax({
     }
 
 })
-
-$(document).ready(function () {
-
-    $("#soft_test_act .soft_ad_content").hover(function () {
-        $(".soft_ad_content").removeClass("active animated pulse");
-        $(this).addClass("active animated pulse");
-    });
-})
 // 六个栏目
 $(document).ready(function () {
     $("#soft_top_tab li").click(function () {
         $(this).addClass("active").siblings().removeClass("active");
     })
-    $("#soft_team_area>div").hover(function () {
-        $(this).addClass("active animated pulse").siblings().removeClass("active animated pulse");
-    });
+
     $("#soft_act_area span").hover(function () {
         $(this).addClass("active").siblings().removeClass("active");
         var index = $(this).index();
@@ -190,8 +181,11 @@ $(document).ready(function () {
         clearTimeout();
     })
     // 模拟测试
-    $("#soft_test_btn").click(function () {
-        window.location.href = "www.baidu.com";
+})
+$(document).ready(function () {
+    $(".question_btn").click(function () {
+        console.log("dianji")
+        $(this).parent().next().removeClass("soft_none")
     })
 })
 // 鼠标滚轮
@@ -251,3 +245,70 @@ $(document).ready(function () {
         };
     }
 })
+/** 
+ * @author xuesong
+ * @time  2019-03-05 
+ */
+function getDocumentTop() {
+    var scrollTop = 0,
+        bodyScrollTop = 0,
+        documentScrollTop = 0;
+    if (document.body) {
+        bodyScrollTop = document.body.scrollTop;
+    }
+    if (document.documentElement) {
+        documentScrollTop = document.documentElement.scrollTop;
+    }
+    scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+    return scrollTop;
+}
+/** 
+ * @author xuesong
+ * @time  2019-03-05 
+ */
+//可视窗口高度
+function getWindowHeight() {
+
+    var windowHeight = 0;
+    if (document.compatMode == "CSS1Compat") {
+        windowHeight = document.documentElement.clientHeight;
+    } else {
+        windowHeight = document.body.clientHeight;
+    }
+    return windowHeight;
+
+}
+/** 
+ * @author xuesong
+ * @time  2019-03-05 
+ */
+//滚动条滚动高度
+function getScrollHeight() {
+    var scrollHeight = 0,
+        bodyScrollHeight = 0,
+        documentScrollHeight = 0;
+    if (document.body) {
+        bodyScrollHeight = document.body.scrollHeight;
+    }
+    if (document.documentElement) {
+        documentScrollHeight = document.documentElement.scrollHeight;
+    }
+    scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+    return scrollHeight;
+}
+window.onscroll = function () {
+    //监听事件内容
+    if (getScrollHeight() == getWindowHeight() + getDocumentTop()) {
+        //当滚动条到底时,这里是触发内容
+        if (!localStorage.user_token) {
+            var password = Date.parse(new Date()).toString() + Math.ceil(Math.random() * 10000).toString();
+            //加密成md5
+            var passwd = $.md5(password);
+            localStorage.setItem("user_token", passwd)
+        }
+        //get请求,用于统计页面访问数
+        $.get("http://192.168.60.175:666/activity/enroll/browse&user_token=" + localStorage.user_token + "&act_token=afuxnsd524d", function (data, status) {
+            // alert("数据：" + data + "\n状态：" + status);
+        });
+    }
+}
